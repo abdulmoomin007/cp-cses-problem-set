dd99ebb8cdc829adc53e651bead488dfc78f6745
abdulmoomin007 <abdulmoomin007@gmail.com>
2025-08-09 17:52:22 +0530

    Solve Nested Ranges Check

---
+++ 2. Sorting and Searching/20. Nested Ranges Check.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+
+// Constants
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// Fast I/O
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Macros
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define all(x) x.begin(), x.end()
+
+// Debug macro
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+
+// GCD & LCM
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve();
+
+int main()
+{
+    FAST_IO;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+        // cout << "\n";
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
+
+// Solve function for each test case
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<pii> a(n);
+    for (pii &x : a)
+    {
+        cin >> x.first;
+        cin >> x.second;
+    }
+    map<pii, int> ind;
+    rep(i, 0, n) ind[a[i]] = i;
+    sort(all(a), [](pii x, pii y)
+         {
+        if(x.first == y.first) return x.second > y.second;
+        return x.first < y.first; });
+    // for (pii x : a)
+    //     cout << x.first << " " << x.second << endl;
+    vector<bool> contains(n), contained(n);
+    int cur = -1;
+    rep(i, 0, n)
+    {
+        if (a[i].second > cur)
+            cur = a[i].second;
+        else
+            contained[ind[a[i]]] = true;
+    }
+
+    cur = INF + 1;
+    reverse(all(a));
+    rep(i, 0, n)
+    {
+        if (a[i].second >= cur)
+            contains[ind[a[i]]] = true;
+        else
+            cur = a[i].second;
+    }
+
+    for (bool x : contains)
+        cout << x << " ";
+    cout << endl;
+    for (bool x : contained)
+        cout << x << " ";
+    cout << endl;
+}
